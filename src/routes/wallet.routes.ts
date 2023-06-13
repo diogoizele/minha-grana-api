59f0eda7c40094260d7686ee6e6c62da67bb1aba
Igor Vinoski <igor.vinoski2@gmail.com>
2023-06-13 19:43:46 -0300

    fix: routes

--- src/routes/wallet.routes.ts
+++ src/routes/wallet.routes.ts
@@ -1,17 +1,11 @@
-import express from "express"; 
+import express from "express";
 import WalletController from "../controllers/wallet.controller";
 
 const router = express.Router();
 
-
-
-router.post("/wallet/create", WalletController.createWallet) ;
-
-router.put("/wallet/update/:id", WalletController.updateWallet);
-
-//
-router.get("/wallets",  WalletController.getWallets);
-
-router.get("/wallet:id",  WalletController.getWallet);
+router.post("/wallets", WalletController.createWallet);
+router.put("/wallets/:id", WalletController.updateWallet);
+router.get("/wallets", WalletController.getWallets);
+router.get("/wallets/:id", WalletController.getWallet);
 
 export default router;
